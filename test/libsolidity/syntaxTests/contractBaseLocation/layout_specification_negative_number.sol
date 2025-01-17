7473a01a6d9b88b39c02a2f842f33006e62e78bd
Matheus Aguiar <matheus.pit@gmail.com>
2025-01-17 11:58:13 -0300

    fixup! add more tests

---
+++ test/libsolidity/syntaxTests/contractBaseLocation/layout_specification_negative_number.sol
@@ -0,0 +1,3 @@
+contract A layout at -1 {}
+// ----
+// TypeError 1763: (21-23): Contract storage base location must be in range of type uint256. Current type is int_const -1 Cannot implicitly convert signed literal to unsigned type.
